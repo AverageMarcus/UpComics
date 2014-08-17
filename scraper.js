4c70e9489a88485b8b7276f35123ca4e8e714aa0
unknown <github@marcusnoble.co.uk>
2014-08-17 06:44:00 +0100

    Added Dark Horse scraping (See #10)

--- scraper.js
+++ scraper.js
@@ -9,6 +9,7 @@ var titleHelper = require('./TitleHelper');
 var baseMarvelURL = 'http://marvel.com';
 var baseImageURL = 'http://imagecomics.com';
 var baseDCURL = 'http://www.dccomics.com';
+var baseDarkHorseURL = 'http://www.darkhorse.com';
 
 var db = mongoose.connection;
 
@@ -158,6 +159,62 @@ db.once('open', function() {
             });
         };
         scrapeDCComics();
+    });
+
+    var darkHorseScrape = schedule.scheduleJob({hour: 8, dayOfWeek: new schedule.Range(1, 5)}, function(){
+        var scrapeDarkHorseComics = function scrapeDarkHorseComics(page){
+            var now = month || moment();
+            var url = baseDarkHorseURL + '/Comics/Upcoming?page=';
+
+            request(url, function(error, response, html){
+                if(!error){
+                    var $ = cheerio.load(html);
+                    
+                    var pages = $('select[name=page]').first().length;
+
+                    for(var i=0;i<= pages;i++){
+                        request(url+i, function(error, response, html){
+                            if(!error){
+                                var $ = cheerio.load(html);
+                                var domComics = $('.list_items_container').children();
+                                if(domComics.length === 0){
+                                    return;
+                                }
+                                var currentDate = "";
+
+                                domComics.each(function(){
+                                    if($(this).is('h3')){
+                                        currentDate = moment.parse($(this).text().trim(), "MMMM DD, YYYY")
+                                    }
+                                    if($(this).hasClass('list_item')){
+                                        var fullTitle = $(this).find('a').last().text().trim();
+                                        var link = baseDarkHorseURL + $(this).find('a').first().attr('href');
+                                        var release_date = currentDate;
+                                        var title = titleHelper.getTitle(fullTitle);
+                                        var issue = titleHelper.getIssue(fullTitle);
+
+                                        var newComic = {
+                                            title: title,
+                                            issue: issue,
+                                            release_date: release_date,
+                                            publisher: 'Dark Horse',
+                                            link : link
+                                        };
+
+                                        Comic.update({ title : newComic.title, publisher: newComic.publisher}, {$set: newComic}, {upsert:true}, function(err, newComic){
+                                            if (err) return console.error(err);
+                                        });
+                                    }
+                                    
+                                });
+
+                            }
+                        });
+                    }
+                }
+            });
+        };
+        scrapeDarkHorseComics();
     });
 });
 
