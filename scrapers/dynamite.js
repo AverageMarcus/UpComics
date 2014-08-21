e770ae35597c4d39d3f4dc4f727ff126630e0533
AverageMarcus <github@marcusnoble.co.uk>
2014-08-21 23:53:56 +0100

    Added dynamite scraper (See #10)

---
+++ scrapers/dynamite.js
@@ -0,0 +1,59 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var schedule = require('node-schedule');
+var mongoose = require('mongoose');
+var Comic = require('../models/Comic').Comic;
+var moment = require('moment');
+var titleHelper = require('../utils/titleHelper');
+
+var baseImageURL = 'http://www.dynamite.com';
+
+var imageScrape = schedule.scheduleJob({hour: 10, dayOfWeek: new schedule.Range(1, 5)}, function(){
+    console.log("About to scrape Dynamite comics");
+    var scrapeDynamiteComics = function scrapeDynamiteComics(month){
+        var now = month || moment();
+        var url = baseImageURL + '/htmlfiles/previews.html?getMonth='+now.format("MMMM")+'&getYear='+now.format("YYYY");
+        console.log("Scraping "+url);
+        request(url, function(error, response, html){
+            if(error) console.error(error);
+            if(!error){
+                console.log("Got a response");
+
+                var $ = cheerio.load(html);
+
+                var releases = [];
+
+                var domComics = $('#indexPlacement tr[align=left]');
+                if(domComics.length === 0){
+                    console.log("Done for now");
+                    return;
+                }
+                var i = 0;
+                domComics.each(function(){
+                    var fullTitle = $(this).find('strong').text().trim();
+                    var link = baseImageURL + $(this).find('a').first().attr('href');
+                    var release_date = moment(html.match(/ON SALE DATE: ([\w]+ [\d+])/i)[i++] + now.format(" YYYY"));
+                    var title = titleHelper.getTitle(fullTitle);
+                    var issue = titleHelper.getIssue(fullTitle);
+                    
+                    var newComic = {
+                        title: title,
+                        issue: issue,
+                        release_date: release_date.format('YYYY-MM-DD'),
+                        publisher: 'Dynamite',
+                        link : link
+                    };
+
+                    Comic.update({ title : newComic.title, publisher: newComic.publisher}, {$set: newComic}, {upsert:true}, function(err, newComic){
+                        if (err) return console.error(err);
+                    });
+                });
+                now.add(1, 'months');
+                scrapeDynamiteComics(now);
+            }
+        });
+    };
+    scrapeDynamiteComics();
+});
+
+console.log("✔ Dynamite scraper loaded");
